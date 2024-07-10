313b5b3cb9f209286d16f10c99e38c0619959947
R <2116804@cognizant.com>
2024-07-10 19:07:31 +0530

    Modified and Implemented the API-GATEWAY

--- background-verification/src/main/java/com/cognizant/backgroundverification/service/BackgroundVerificationService.java
+++ background-verification/src/main/java/com/cognizant/backgroundverification/service/BackgroundVerificationService.java
@@ -4,7 +4,7 @@ package com.cognizant.backgroundverification.service;
 import com.cognizant.backgroundverification.dto.EmployeeVerificationRequest;
 import org.springframework.stereotype.Service;
 
-@Service
+
 public interface BackgroundVerificationService {
     public String verifyEmployeeBackground(EmployeeVerificationRequest employeeVerificationRequest);
 }
